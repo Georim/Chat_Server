d12230927b1af49c4b797f81080f43b4888a40f6
GEORIM <georim89@mail.ru>
2015-06-02 21:39:10 +0300

    Initial commit

---
+++ src/main/java/ru/georimstudio/Server.java
@@ -0,0 +1,11 @@
+package ru.georimstudio;
+
+/**
+ * Created by Oberon on 02.06.2015.
+ */
+public class Server {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
